0f5e8ad19a4b912c9a2bf54b2c3309e36aed305e
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2025-01-16 11:19:24 -0800

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './components/marketingapp';
+
+export default () => {
+    return <div>
+        <h1>Howdy There!!!!</h1>
+        <hr />
+        <MarketingApp />
+    </div>
+};
