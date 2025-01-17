2bb5c6fd340cdc160d044b476f242fdfc4630690
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2025-01-16 17:18:12 -0800

    created container deploy script

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+    name: "marketing",
+    output: {
+        filename: "[name].[outputhash].js"
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "marketing",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./MarketingApp": "./src/bootstrap"
+            },
+            shared: packageJson.dependencies
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
